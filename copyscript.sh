821567053d7819d9fa9aa57cf8f642b8abad136b
Phil Stark <philipcstark@gmail.com>
2013-08-09 06:59:23 -0600

    Update DEVELOPMENT to v1.0.17
    v1.0.16 set for release.

--- copyscript.sh
+++ copyscript.sh
@@ -2,7 +2,7 @@
 # Original version written by Phil Stark
 # Maintained and updated by Phil Stark and Blaine Motsinger
 #
-VERSION="1.0.16"
+VERSION="1.0.17"
 scripthome="/root/.copyscript"
 #
 # Purpose:  to find all accounts existing on the Source server that do not exist
